d21b4618c0207ee187756b905008a248499beac3
rayzhiguangwang <15242605952@163.com>
2016-04-13 17:40:42 +1200

    create migration database tables for users, routes, driverroutes, trips

---
+++ database/migrations/2016_04_13_050044_create_driverroutes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriverroutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driverroutes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('driverid')->unsigned();
+            $table->foreign("driverid")->references('id')->on('users');
+            $table->integer("routeid")->unsigned();
+            $table->foreign("routeid")->references('id')->on('routes');
+            $table->float('price');
+            $table->string('capacity');
+            $table->boolean("offered");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('driverroutes');
+    }
+}
---
+++ database/migrations/2016_04_13_053105_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("driverrouteid")->unsigned();
+            $table->foreign("driverrouteid")->references("id")->on('driverroutes');
+            $table->float("posterprice");
+            $table->integer("status");
+            $table->date("requestdate");
+            $table->date("startdate");
+            $table->date("enddate");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('trips');
+    }
+}
